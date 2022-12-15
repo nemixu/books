0c374589c497e019cf8261f5463950cd5f71bd20
nemixu <s.r.seagrave@gmail.com>
2022-12-15 18:10:16 +0000

    Add custom hook to usebooks context through application

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,14 +1,11 @@
 import BookShow from "./BookShow";
 import React from "react";
-import { useContext } from "react";
-import BooksContext from "../context/books";
+import useBooksContext from "../hooks/use-books-context";
 
 function BookList() {
-  const { books, } = useContext(BooksContext);
+  const { books } = useBooksContext();
   const renderedBooks = books.map((book) => {
-    return (
-      <BookShow key={book.id} book={book}/>
-    );
+    return <BookShow key={book.id} book={book} />;
   });
 
   return <div className="book-list">{renderedBooks}</div>;
