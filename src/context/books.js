664ac3666c4b6130da501f7239124c4c9d4e539c
nemixu <s.r.seagrave@gmail.com>
2022-12-19 13:47:09 +0000

    add usecallback to fix useeffect issues

--- src/context/books.js
+++ src/context/books.js
@@ -1,15 +1,15 @@
 import axios from "axios";
-import { createContext, useState } from "react";
+import { createContext, useState, useCallback } from "react";
 
 const BooksContext = createContext();
 
 function Provider({ children }) {
   const [books, setBooks] = useState([]);
 
-  const fetchBooks = async () => {
+  const fetchBooks = useCallback(async () => {
     const response = await axios.get("http://localhost:3001/books");
     setBooks(response.data);
-  };
+  }, []);
 
   const editBookById = async (id, newTitle) => {
     const response = await axios.put(`http://localhost:3001/books/${id}`, {
